115b941f529cf7493f2ad386e3fb13b0afde90db
Alexander Galushkin <ajugalushkin@gmail.com>
2022-04-04 19:16:08 +0300

    asasc

--- Program.cs
+++ Program.cs
@@ -1,22 +1,23 @@
 ﻿using System.IO;
 
-string Doljnost = Console.ReadLine();
-
 string[] massInput = File.ReadAllLines("A:\\Users\\ajuga\\source\\repos\\LearnGit\\employees.dat");
 
-string[] massOutput;
+foreach (var item in massInput) {
+    Console.WriteLine(item);
+}
 
-for(int i = 0; i < massInput.Length){
+string Doljnost = Console.ReadLine();
 
-string[] currentStringMass = massInput[i].Split('|');
+List<string> massOutput = new List<string>();
+for (int i = 0; i < massInput.Length; i++) {
+    string[] currentStringMass = massInput[i].Split('|');
 
-if (Doljnost == currentStringMass[1]){
-    massOutput = currentStringMass[0] + " " + currentStringMass[2];
+    if (Doljnost == currentStringMass[1]) {
+        massOutput.Add(currentStringMass[0] + " " + currentStringMass[2]);
+    }
 }
 
 foreach( var item in massOutput){
     Console.Write(item);
 }
 Console.WriteLine();
-
-}
