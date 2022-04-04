80107b5e36f8826b72b1e7b41f3fe8ce2487b203
Alexander Galushkin <ajugalushkin@gmail.com>
2022-04-04 18:56:44 +0300

    Solve task

--- Program.cs
+++ Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.IO;
+
+string Doljnost = Console.ReadLine();
+
+string[] massInput = File.ReadAllLines("A:\\Users\\ajuga\\source\\repos\\LearnGit\\employees.dat");
+
+string[] massOutput;
+
+for(int i = 0; i < massInput.Length){
+
+string[] currentStringMass = massInput[i].Split('|');
+
+if (Doljnost == currentStringMass[1]){
+    massOutput = currentStringMass[0] + " " + currentStringMass[2];
+}
+
+foreach( var item in massOutput){
+    Console.Write(item);
+}
+Console.WriteLine();
+
+}
